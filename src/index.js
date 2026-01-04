0909c42980590d51cb10f1cb0416ef2164abd964
Alex Ndaula <your_email@example.com>
2026-01-04 15:41:52 -0500

    Fetch movie details on demand via form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
+  const form = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector("#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("#movieDetails h4");
+        const summary = document.querySelector("#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
